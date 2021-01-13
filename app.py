7653cb81bba8ee311cbb6bb9c2df986347414916
Clark Ke <clark1990@me.com>
2021-01-13 12:07:11 +0800

    add index page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask import render_template
 from flask import url_for,escape #用户输入的数据会包含恶意代码，需要使用 Flask 提供的 escape() 函数对 name 变量进行转义处理
+# 为了避免手写，Flask 提供了一个 url_for 函数来生成 URL，它接受的第一个参数就是端点值，默认为视图函数的名称
 app = Flask(__name__)
+
 app.debug = True
 
+#一个视图函数也可以绑定多个 URL，这通过附加多个装饰器实现
 @app.route ('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -24,6 +28,19 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+name = 'Clark'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 if __name__ == "__main__":
     app.debug = True
